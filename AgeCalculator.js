fd1b97509de58a2bbbadcddea77fccf06f5a057f
Zia ullah <itstar2057@users.noreply.github.com>
2021-04-04 00:01:28 +0500

    Add files via upload

---
+++ AgeCalculator.js
@@ -0,0 +1,51 @@
+//variables for date picker
+var datePicker = document.getElementById('datePicker');
+var choseDate = document.getElementById('choseDate');
+
+// variables for printing the given values
+var ageYear = document.getElementById('ageYear');
+var ageMonth = document.getElementById('ageMonth');
+var ageDays = document.getElementById('ageDays');
+var ageHours = document.getElementById('ageHours');
+var ageSeconds  = document.getElementById('ageSeconds');
+var ageMiliSeconds = document.getElementById('ageMiliseconds');
+
+datePicker.addEventListener('change',function(){
+    var options = {year: 'numeric', month: 'long', day: 'numeric'};
+    var selectedDate = new Date(this.value);
+    var DOB = selectedDate.toLocaleDateString('en-US', options);
+
+    choseDate.innerHTML = "DOB : " + " " + DOB;
+
+    var miliSeconds_Btw_DOB = Date.parse(DOB);
+    var miliSeconds_Btw_Now = Date.now();
+
+    var age_in_MiliSeconds = miliSeconds_Btw_Now - miliSeconds_Btw_DOB;
+
+    var miliSeconds = age_in_MiliSeconds;
+    var second = 1000;
+    var minute = second*60;
+    var hour = minute*60;
+    var day = hour*24;
+    var month = day*30;
+    var year = day*365;
+
+    // Now start the calculation
+
+    var years = Math.round(miliSeconds/year);
+    var months = years*12;
+    var days = years*365;
+    var hours = Math.round(miliSeconds/hour);
+    var seconds = Math.round(miliSeconds/second);
+
+    // Now to Print the Values in boxes
+ageYear.innerHTML = years;
+ageMonth.innerHTML = months; 
+ageDays.innerHTML = days;
+ageHours.innerHTML = hours;
+ageSeconds.innerHTML = seconds;
+ageMiliSeconds.innerHTML = miliSeconds;
+
+//document.querySelector('.age-calc').classList.add('expand');
+
+})
